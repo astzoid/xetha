fdee587af9e2a49f6090bb8047cdefd747dbc2b9
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-04-19 13:45:52 +0800

    style: 2 spaces instead of 4

--- athena/snowpack.config.js
+++ athena/snowpack.config.js
@@ -7,58 +7,58 @@ const proxy = httpProxy.createServer({ target: 'http://localhost:3001' });
 
 /** @type {import('snowpack').SnowpackUserConfig} */
 module.exports = {
-    mount: {
-        public: {
-            url: '/',
-            static: true,
-        },
-        src: {
-            url: '/dist',
-        },
+  mount: {
+    public: {
+      url: '/',
+      static: true,
     },
-    plugins: [
-        '@snowpack/plugin-react-refresh',
-        '@snowpack/plugin-dotenv',
-        '@snowpack/plugin-typescript',
-        ['@snowpack/plugin-webpack', {
-            /**
-             * 
-             * @param {import('webpack').Configuration} config 
-             * @returns 
-             */
-            extendConfig: (config) => {
-                config.plugins.push(new SubresourceIntegrity({ enabled: true, hashFuncNames: ['sha384'] }));
-                config.output.crossOriginLoading = 'anonymous';
-                return config;
-            },
-            sourceMap: false,
-            outputPattern: {
-                assets: 'static/assets/[name].[contenthash:8].[ext]',
-                css: 'static/css/[name].[contenthash:8].css',
-                js: 'static/js/[name].[contenthash:8].js',
-            }
-        }],
-    ],
-    packageOptions: {
-        /* ... */
+    src: {
+      url: '/dist',
     },
-    devOptions: {
-        port: 3000
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+    ['@snowpack/plugin-webpack', {
+      /**
+       * 
+       * @param {import('webpack').Configuration} config 
+       * @returns 
+       */
+      extendConfig: (config) => {
+        config.plugins.push(new SubresourceIntegrity({ enabled: true, hashFuncNames: ['sha384'] }));
+        config.output.crossOriginLoading = 'anonymous';
+        return config;
+      },
+      sourceMap: false,
+      outputPattern: {
+        assets: 'static/assets/[name].[contenthash:8].[ext]',
+        css: 'static/css/[name].[contenthash:8].css',
+        js: 'static/js/[name].[contenthash:8].js',
+      }
+    }],
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    port: 3000
+  },
+  buildOptions: {
+    out: 'dist',
+  },
+  routes: [
+    {
+      src: '/api/.*',
+      dest: (req, res) => {
+        proxy.web(req, res);
+      },
     },
-    buildOptions: {
-        out: 'dist',
+    {
+      match: "routes",
+      src: ".*",
+      dest: "/index.html",
     },
-    routes: [
-        {
-            src: '/api/.*',
-            dest: (req, res) => {
-                proxy.web(req, res);
-            },
-        },
-        {
-            match: "routes",
-            src: ".*",
-            dest: "/index.html",
-        },
-    ],
+  ],
 };
