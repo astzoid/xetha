9b1e009840714917d794e22cc1d54b22bd987616
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-04-25 20:57:39 +0800

    fix: removed some functions

--- cathy/src/middlewares/gateway.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-
-export default function gateway(req: Request, res: Response, next: () => void) {
-  if (!req.isAuthenticated())
-    return res.status(401).json({ message: 'Not Authorized' });
-
-  next();
-}
