5515e42987c50adc7845475863df9e01e58a4519
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-04-25 12:50:03 +0800

    refactor(Theme): move Theme.ts to index.tsx

--- athena/src/index.tsx
+++ athena/src/index.tsx
@@ -2,23 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { blue, grey } from '@material-ui/core/colors';
 
 import App from './App';
-import ScrollToTop from './shared/components/ScrollToTop';
-import Theme from './shared/styles/Theme';
-import './shared/styles/App.css';
+import ScrollToTop from '@shared/components/ScrollToTop';
+import '@shared/styles/App.css';
+
+const Theme = createMuiTheme({
+  palette: {
+    common: {
+      black: '#101010',
+      white: '#f5f5f5',
+    },
+    type: 'dark',
+    primary: blue,
+    secondary: grey,
+    text: {
+      primary: '#f5f5f5',
+      secondary: '#9e9e9e',
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <HelmetProvider>
+    <HelmetProvider>
+      <BrowserRouter>
         <ThemeProvider theme={Theme}>
           <ScrollToTop />
           <App />
         </ThemeProvider>
-      </HelmetProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById('app-mount'),
 );
