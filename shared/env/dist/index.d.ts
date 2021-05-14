90a8c24515ff58091af07f25c60cbaf0e2cfe09b
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-14 12:20:17 +0800

    chore: @shared/env dist

--- shared/env/dist/index.d.ts
+++ shared/env/dist/index.d.ts
@@ -1,3 +1,12 @@
 export declare const production: boolean;
 export declare const development: boolean;
+export declare const environment: {
+    CLIENT_ID: string | void;
+    CLIENT_SECRET: string | void;
+    DISCORD_TOKEN: string | void;
+    JWT_SECRET: string | void;
+    MONGODB_URI: string;
+    WEBSITE_URL: string;
+    REDIRECT_URI: string;
+};
 //# sourceMappingURL=index.d.ts.map
--- shared/env/dist/index.js
+++ shared/env/dist/index.js
@@ -1,11 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.development = exports.production = void 0;
+exports.environment = exports.development = exports.production = void 0;
 const tslib_1 = require("tslib");
 const dotenv_1 = tslib_1.__importDefault(require("dotenv"));
 dotenv_1.default.config();
 process.env.NODE_ENV =
     process.platform === 'linux' ? 'production' : 'development';
+function throwError(property) {
+    console.error(new Error(`Environment Variable '${property}' was not specified.`));
+    process.exit(1);
+}
 exports.production = process.env.NODE_ENV === 'production';
 exports.development = process.env.NODE_ENV === 'development';
+exports.environment = {
+    CLIENT_ID: process.env.CLIENT_ID ?? throwError('CLIENT_ID'),
+    CLIENT_SECRET: process.env.CLIENT_SECRET ?? throwError('CLIENT_SECRET'),
+    DISCORD_TOKEN: process.env.DISCORD_TOKEN ?? throwError('DISCORD_TOKEN'),
+    JWT_SECRET: process.env.JWT_SECRET ?? throwError('JWT_SECRET'),
+    MONGODB_URI: process.env.MONGOD_URI ?? 'mongodb://localhost/xetha',
+    WEBSITE_URL: process.env.WEBSITE_URL ?? 'http://localhost:3000',
+    REDIRECT_URI: process.env.REDIRECT_URI ?? 'http://localhost:3001/api/redirect',
+};
 //# sourceMappingURL=index.js.map
