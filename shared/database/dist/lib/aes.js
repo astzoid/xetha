c50e63b045e19c3459e980ff91ba9e0a4aa0b436
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-15 16:53:32 +0800

    fix: encryption and decryption

--- shared/database/dist/lib/aes.js
+++ shared/database/dist/lib/aes.js
@@ -3,16 +3,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.decrypt = exports.encrypt = void 0;
 const tslib_1 = require("tslib");
 const crypto_1 = tslib_1.__importDefault(require("crypto"));
-const key = process.env.JWT_SECRET ?? 'Were in development';
+const secret = process.env.JWT_SECRET ?? 'Were in development';
+const key = crypto_1.default
+    .createHash('sha256')
+    .update(String(secret))
+    .digest('base64')
+    .substr(0, 32);
 function encrypt(val) {
-    const cipher = crypto_1.default.createCipheriv('aes-256-cbc', key + key, key);
+    const cipher = crypto_1.default.createCipheriv('aes-256-cbc', key, key.slice(0, 16));
     let encrypted = cipher.update(val, 'utf8', 'base64');
     encrypted += cipher.final('base64');
-    return encrypted;
+    return encrypted.toString();
 }
 exports.encrypt = encrypt;
 function decrypt(encrypted) {
-    const decipher = crypto_1.default.createDecipheriv('aes-256-cbc', key + key, key);
+    const decipher = crypto_1.default.createDecipheriv('aes-256-cbc', key, key.slice(0, 16));
     const decrypted = decipher.update(encrypted, 'base64', 'utf8');
     return decrypted + decipher.final('utf8');
 }
--- shared/database/src/lib/aes.ts
+++ shared/database/src/lib/aes.ts
@@ -1,16 +1,25 @@
 import crypto from 'crypto';
 
-const key = process.env.JWT_SECRET ?? 'Were in development';
+const secret = process.env.JWT_SECRET ?? 'Were in development';
+const key = crypto
+    .createHash('sha256')
+    .update(String(secret))
+    .digest('base64')
+    .substr(0, 32);
 
 export function encrypt(val: string) {
-    const cipher = crypto.createCipheriv('aes-256-cbc', key + key, key);
+    const cipher = crypto.createCipheriv('aes-256-cbc', key, key.slice(0, 16));
     let encrypted = cipher.update(val, 'utf8', 'base64');
     encrypted += cipher.final('base64');
-    return encrypted;
+    return encrypted.toString();
 }
 
 export function decrypt(encrypted: string) {
-    const decipher = crypto.createDecipheriv('aes-256-cbc', key + key, key);
+    const decipher = crypto.createDecipheriv(
+        'aes-256-cbc',
+        key,
+        key.slice(0, 16),
+    );
     const decrypted = decipher.update(encrypted, 'base64', 'utf8');
     return decrypted + decipher.final('utf8');
 }
