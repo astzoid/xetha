207f716aa3a8f49ae4d1852f7881cbb08cd88481
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-15 14:01:23 +0800

    feat: dynamic bot invite links

--- athena/src/routes/Redirects/Invite.tsx
+++ athena/src/routes/Redirects/Invite.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import createInviteLink from '@functions/createInviteLink';
+import useQueryParams from '@hooks/useQueryParams';
 
 export default function Invite() {
-    const location = useLocation();
-    let guild_id: string | null;
-    try {
-        guild_id = new URLSearchParams(location.search).get('guild_id');
-    } catch (err) {
-        guild_id = null;
-    }
-    window.location.href = `/api/invite${
-        guild_id ? `?guild_id=${guild_id}` : ''
-    }`;
+    const query = useQueryParams();
+    window.location.href = createInviteLink(query.get('guild_id'));
     return <></>;
 }
