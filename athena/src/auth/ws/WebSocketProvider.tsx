d8403af95d77e22275773d694fca4d20a044b483
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-16 16:13:15 +0800

    feat(hooks): useLoading

--- athena/src/auth/ws/WebSocketProvider.tsx
+++ athena/src/auth/ws/WebSocketProvider.tsx
@@ -1,32 +1,31 @@
 import React, { createContext, useState, useEffect } from 'react';
 import WebSocketClient from '@auth/ws/WebSocketClient';
 
+import Logger from '@functions/Logger';
+
 import useQueryParams from '@hooks/useQueryParams';
 import useSaveTokens from '@hooks/useSaveTokens';
 import useTokenStore from '@hooks/useTokenStore';
 
-import type { ReactNode } from 'react';
+import type Tokens from '@typings/Tokens';
 import type User from '@typings/User';
 
 interface Context {
     client: WebSocketClient | null;
     user: User | null;
+    loading: boolean;
 }
 
 export const WebSocketContext = createContext<Context>({
     client: null,
     user: null,
+    loading: true,
 });
 
-interface Props {
-    fallback: ReactNode;
-    children: ReactNode;
-}
-
-const WebSocketProvider: React.FC<Props> = (props) => {
+const WebSocketProvider: React.FC = (props) => {
     const [client, setClient] = useState<WebSocketClient | null>(null);
     const [user, setUser] = useState<User | null>(null);
-    const [serverDown, setServerDown] = useState(false);
+    const [loading, setLoading] = useState(true);
     const query = useQueryParams();
 
     useSaveTokens();
@@ -36,32 +35,35 @@ const WebSocketProvider: React.FC<Props> = (props) => {
             query.has('accessToken') && query.has('refreshToken')
                 ? false
                 : true;
+
         if (connect) {
-            new WebSocketClient()
-                .listen({
-                    onUser: (user) => {
-                        setUser(user);
-                    },
-                    onToken: (tokens) => {
-                        useTokenStore.getState().setTokens(tokens);
-                    },
-                    onConnectionFailed: () => {
-                        setServerDown(true);
-                    },
-                })
-                .connect(useTokenStore.getState())
-                .then((ws) => {
-                    setClient(ws);
-                });
+            const ws = new WebSocketClient();
+
+            ws.socket.on('connect', () => {
+                Logger.log('WS', `[CONNECTED] ${ws.socket.id}`);
+                ws.socket.emit('auth', useTokenStore.getState());
+            });
+
+            ws.socket.on('auth:success', (user: User | null) => {
+                if (user) Logger.log('WS', `Logged in as ${user.user_id}`);
+                setUser(user || null);
+                setLoading(false);
+            });
+
+            ws.socket.on('auth:token', (tokens: Tokens) => {
+                useTokenStore.getState().setTokens(tokens);
+            });
+
+            ws.socket.connect();
+
+            setClient(ws);
         }
     }, []);
 
     if (!client) return null;
 
-    if (serverDown) return <>{props.fallback}</>;
-
     return (
-        <WebSocketContext.Provider value={{ client, user }}>
+        <WebSocketContext.Provider value={{ client, user, loading }}>
             {props.children}
         </WebSocketContext.Provider>
     );
