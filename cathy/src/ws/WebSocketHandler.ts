c3434caf9245cdea52d2579a202bb6b4c573d520
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-16 21:06:43 +0800

    fix: use Listeners

--- cathy/src/ws/WebSocketHandler.ts
+++ cathy/src/ws/WebSocketHandler.ts
@@ -1,11 +1,12 @@
 import AuthHandler from './AuthHandler';
 import Logger from '../utils/Logger';
+import Listeners from './Listeners';
 import type { Socket } from 'socket.io';
 import type { Tokens } from './Tokenizer';
 
 export default function WebSocketHandler(socket: Socket) {
     Logger.info(`[client] ${socket.id} connected.`);
-
+    Listeners(socket);
     let authenticated = false;
 
     socket.on('auth', async (tokens: Tokens) => {
